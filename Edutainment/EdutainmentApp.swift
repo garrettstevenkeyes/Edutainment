d52b3e9f501ca0ca0c7772a89a14a684d1fffd6f
Garrett Keyes <garrett.keyes@gmail.com>
2025-11-18 21:32:27 -0500

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Garrett Keyes on 11/18/25.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
